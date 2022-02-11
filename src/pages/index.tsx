981e27134833e7ec75e1e0aa1fb03befaf3050e6
paulotiba <tibapaulo@outlook.com>
2022-02-11 10:01:18 -0300

    startProject

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <div className={` 
+    flex justify-center items-center text-7xl
+    h-screen
+    bg-gradient-to-r from-green-500 to-blue-500
+    `}>
+      <h1>admin</h1>
+    </div>
+  )
+}
